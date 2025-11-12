36071783510620e3b2000f12760d1f949eb649e5
nicoguinzio <nicolasguinzio@gmail.com>
2025-11-12 20:05:08 -0300

    Nico empieza

--- src/main/java/com/alassia/guinizio/policiafederal/PoliciaFederalApplication.java
+++ src/main/java/com/alassia/guinizio/policiafederal/PoliciaFederalApplication.java
@@ -8,6 +8,6 @@ public class PoliciaFederalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoliciaFederalApplication.class, args);
+        System.out.println("Hola Mundo! Como va todo?");
 	}
-
 }
