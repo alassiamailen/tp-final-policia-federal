f21032468d7281cf9f6db15fcc531d2729e59083
alassiamailen <alassiamailen3@gmail.com>
2025-11-12 09:01:45 -0300

    conexion con db

---
+++ src/main/java/com/alassia/guinizio/policiafederal/PoliciaFederalApplication.java
@@ -0,0 +1,13 @@
+package com.alassia.guinizio.policiafederal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliciaFederalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliciaFederalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alassia/guinizio/policiafederal/PoliciaFederalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alassia.guinizio.policiafederal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliciaFederalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
